78605fee7b63fdb74c68f8190a6417a2b3807487
Arsen <arsendada@mail.ru>
2022-11-19 19:28:10 +0300

    created class PostAdmin

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    authors = models.ForeignKey(User,
+    author = models.ForeignKey(User,
                                 on_delete=models.CASCADE,
                                 related_name='blog_posts')
     body = models.TextField()
