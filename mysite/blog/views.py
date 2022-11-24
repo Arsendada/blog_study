178296c464fcc60bf1473b5cc9cf854ca81000fc
Arsen <arsendada@mail.ru>
2022-11-24 19:56:23 +0300

    created views function post_comment

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,9 +3,10 @@ from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from django.views.decorators.http import require_POST
 
-from .models import Post
+from .forms import *
+from .models import *
 from mysite.settings import EMAIL_HOST_USER
 
 
@@ -71,3 +72,18 @@ def post_share(request, post_id):
          'form': form,
          'sent': sent}
     )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
